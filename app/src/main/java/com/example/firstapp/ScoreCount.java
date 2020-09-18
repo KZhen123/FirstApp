745b316f3e47d4de1a25b4487838e0a248f651cc
kongzhen <2625882561@qq.com>
2020-09-18 16:10:17 +0800

    new class:ScoreCount

--- app/src/main/java/com/example/firstapp/ScoreCount.java
+++ app/src/main/java/com/example/firstapp/ScoreCount.java
@@ -13,23 +13,22 @@ public class ScoreCount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_3);
+        score=findViewById(R.id.textView10);
 
-        score=(TextView)findViewById(R.id.textView10);
-
-        Button btn=(Button)findViewById(R.id.button3);
+        Button btn=findViewById(R.id.button3);
         btn.setOnClickListener(new ButtonListener());
-        Button btn2=(Button)findViewById(R.id.button4);
+        Button btn2=findViewById(R.id.button4);
         btn2.setOnClickListener(new ButtonListener());
-        Button btn3=(Button)findViewById(R.id.button5);
+        Button btn3=findViewById(R.id.button5);
         btn3.setOnClickListener(new ButtonListener());
-        Button btn4=(Button)findViewById(R.id.button6);
+        Button btn4=findViewById(R.id.button6);
         btn4.setOnClickListener(new ButtonListener());
     }
     private class ButtonListener implements View.OnClickListener{
         public void onClick(View v){
             String str=score.getText().toString();
             int sc=Integer.valueOf(str);
-            String outtext=null;
+            String outtext;
             switch (v.getId()){
                 case R.id.button3:
                     sc=sc+3;
